fe1e13bb4eb526fc9800302048e8dfb82da8f1f7
Arturas Malinovskis <arturas.malinovskis@konghq.com>
2019-08-14 12:06:10 -0700

    chore(*) add configuration through environment variables

--- kongql-server.js
+++ kongql-server.js
@@ -4,23 +4,30 @@ const app = express();
 const { ApolloServer } = require('apollo-server-express');
 const graphQLSchema = require('swagger-to-graphql');
 
+// configuration parameters
 const pathToSwaggerSchema = process.env.GRAPHQL_SWAGGER_URL;
+const serverPort = process.env.GRAPHQL_SERVER_PORT || 3009;
+const serverPath = process.env.GRAPHQL_SERVER_PATH || '/graphql';
+const playground = process.env.GRAPHQL_PLAYGROUND || false;
 const customHeaders = {
  // Authorization: 'Basic YWRkOmJhc2ljQXV0aA==',
 };
 
 graphQLSchema(pathToSwaggerSchema, null, customHeaders)
   .then(schema => {
-    const apollo = new ApolloServer({ schema })
-    const app = express()
+    const apollo = new ApolloServer({ 
+      schema,
+      playground,
+    });
+    const app = express();
     
-    apollo.applyMiddleware({ app })
+    apollo.applyMiddleware({ app, path: serverPath });
    
-    app.listen({ port: 3009 }, () =>
-      console.log(`🚀 Server ready at http://localhost:3009/graphql`)
-    ) 
+    app.listen({ port: serverPort }, () => {
+      console.log(`🚀 Server ready at http://localhost:${serverPort}${serverPath}`);
+    }); 
   })
   .catch(e => {
     console.log(e);
-    process.exit(1)
+    process.exit(1);
 });
